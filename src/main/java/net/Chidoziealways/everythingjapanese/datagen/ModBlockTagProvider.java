351823a5c6262cfa5723632bd13eefcb742731f3
Chidoziealways <uzowuluchidozie@gmail.com>
2024-09-14 14:29:08 +0100

    Finished DataGen and readded Chocolate Cake

--- src/main/java/net/Chidoziealways/everythingjapanese/datagen/ModBlockTagProvider.java
+++ src/main/java/net/Chidoziealways/everythingjapanese/datagen/ModBlockTagProvider.java
@@ -1,4 +1,31 @@
 package net.Chidoziealways.everythingjapanese.datagen;
 
-public class ModBlockTagProvider {
+import net.Chidoziealways.everythingjapanese.EverythingJapanese;
+import net.Chidoziealways.everythingjapanese.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, EverythingJapanese.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+    tag(BlockTags.MINEABLE_WITH_PICKAXE)
+            .add(ModBlocks.RAW_PYRITE_BLOCK.get())
+            .add(ModBlocks.PYRITE_BLOCK.get())
+            .add(ModBlocks.PYRITE_DEEPSLATE_ORE.get())
+            .add(ModBlocks.PYRITE_ORE.get());
+
+   tag(BlockTags.NEEDS_DIAMOND_TOOL)
+          .add(ModBlocks.PYRITE_ORE.get())
+           .add(ModBlocks.RAW_PYRITE_BLOCK.get());
+   }
 }
--- src/main/java/net/Chidoziealways/everythingjapanese/datagen/ModItemTagProvider.java
+++ src/main/java/net/Chidoziealways/everythingjapanese/datagen/ModItemTagProvider.java
@@ -1,11 +1,31 @@
 package net.Chidoziealways.everythingjapanese.datagen;
 
+
 import net.Chidoziealways.everythingjapanese.EverythingJapanese;
 import net.Chidoziealways.everythingjapanese.item.ModItems;
+import net.Chidoziealways.everythingjapanese.util.ModTags;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
 
-public class ModItemTagProvider  {
+import java.util.concurrent.CompletableFuture;
 
+public class ModItemTagProvider extends ItemTagsProvider {
+
+    public ModItemTagProvider(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> completableFuture,
+                              CompletableFuture<TagLookup<Block>> lookupCompletableFuture, @Nullable ExistingFileHelper existingFileHelper) {
+        super(pOutput, completableFuture, lookupCompletableFuture, EverythingJapanese.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+    tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+            .add(ModItems.PYRITE_INGOT.get())
+            .add(ModItems.RAW_PYRITE.get())
+            .add(ModItems.PYRITE_SWORD.get())
+            .add(ModItems.YA.get());
+    }
 }
